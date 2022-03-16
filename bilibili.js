0acb9839fc2ae0a125094c10ac15aec556dc06e6
Eric Liu <eric1932@users.noreply.github.com>
2022-03-16 14:33:51 -0500

    哔哩哔哩视频流去广告

---
+++ bilibili.js
@@ -0,0 +1,29 @@
+/*
+哔哩哔哩 去除视频流广告
+
+***************************
+QuantumultX:
+
+[rewrite_local]
+^https?:\/\/app\.bilibili\.com\/x\/v2\/feed\/index\/story url script-response-body https://github.com/eric1932/QuantumultX-Scripts/raw/main/bilibili.js
+
+[mitm]
+hostname = app.bilibili.com
+
+***************************
+Surge4 or Loon:
+
+[Script]
+http-response ^https?:\/\/app\.bilibili\.com\/x\/v2\/feed\/index\/story requires-body=1,max-size=0,script-path=https://github.com/eric1932/QuantumultX-Scripts/raw/main/bilibili.js
+
+[MITM]
+hostname = app.bilibili.com
+
+**************************/
+
+var body = JSON.parse($response.body);
+// body: {data: {items: [idx: {ad_info & ad_type}]}}
+if (body.data && body.data.items) {
+    body.data.items = body.data.items.filter(item => item.ad_info === undefined && item.ad_type === undefined)
+}
+$done({body: JSON.stringify(body)}); 
