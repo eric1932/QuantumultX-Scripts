7f43c1c1c37eb5fd8dcebb770207ac3c8d821d8f
Eric <eric1932777@gmail.com>
2022-03-22 15:19:39 -0500

    [feat] Filter by author substring

--- bilibili_feed_index.js
+++ bilibili_feed_index.js
@@ -21,11 +21,22 @@ hostname = app.bilibili.com
 
 **************************/
 
+/* refs
+https://stackoverflow.com/a/5582621/8448191
+*/
+
+const author = [
+    '共青团',
+];
+const authorReg = new RegExp(author.join('|'));
+
 var body = JSON.parse($response.body);
 // body: {data: {items: [{..., ad_info: {...}}]}}
 if (body.data && body.data.items) {
     body.data.items = body.data.items.filter(
-        item => item.ad_info === undefined
+        item =>
+            item.ad_info === undefined  // ad_info not exist
+            && !authorReg.test(item.args.up_name)  // does not match author blacklist
     )
-}
+};
 $done({ body: JSON.stringify(body) });
