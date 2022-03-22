3cfbcb2094f1563027565f6a3ad34f268702c576
Eric <eric1932777@gmail.com>
2022-03-22 14:47:15 -0500

    Rename

--- bilibili_feed_story.js
+++ bilibili_feed_story.js
@@ -5,7 +5,7 @@
 QuantumultX:
 
 [rewrite_local]
-^https?:\/\/app\.bilibili\.com\/x\/v2\/feed\/index\/story url script-response-body https://github.com/eric1932/QuantumultX-Scripts/raw/main/bilibili.js
+^https?:\/\/app\.bilibili\.com\/x\/v2\/feed\/index\/story url script-response-body https://github.com/eric1932/QuantumultX-Scripts/raw/main/bilibili_feed_story.js
 
 [mitm]
 hostname = app.bilibili.com
@@ -14,7 +14,7 @@ hostname = app.bilibili.com
 Surge4 or Loon:
 
 [Script]
-http-response ^https?:\/\/app\.bilibili\.com\/x\/v2\/feed\/index\/story requires-body=1,max-size=0,script-path=https://github.com/eric1932/QuantumultX-Scripts/raw/main/bilibili.js
+http-response ^https?:\/\/app\.bilibili\.com\/x\/v2\/feed\/index\/story requires-body=1,max-size=0,script-path=https://github.com/eric1932/QuantumultX-Scripts/raw/main/bilibili_feed_story.js
 
 [MITM]
 hostname = app.bilibili.com
