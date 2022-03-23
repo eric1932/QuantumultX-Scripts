dd22193601e04d1b7c896d06b52e1afe36c61d13
Eric <eric1932777@gmail.com>
2022-03-23 12:20:45 -0500

    Update MITM hostname

--- bilibili_feed_story.js
+++ bilibili_feed_story.js
@@ -8,7 +8,7 @@ QuantumultX:
 ^https?:\/\/ap(p|i)\.bili(bili|api)\.(com|net)\/x\/v2\/feed\/index\/story url script-response-body https://github.com/eric1932/QuantumultX-Scripts/raw/main/bilibili_feed_story.js
 
 [mitm]
-hostname = app.bilibili.com
+hostname = ap?.biliapi.net, ap?.bilibili.com
 
 ***************************
 Surge4 or Loon:
@@ -17,7 +17,7 @@ Surge4 or Loon:
 http-response ^https?:\/\/ap(p|i)\.bili(bili|api)\.(com|net)\/x\/v2\/feed\/index\/story requires-body=1,max-size=0,script-path=https://github.com/eric1932/QuantumultX-Scripts/raw/main/bilibili_feed_story.js
 
 [MITM]
-hostname = app.bilibili.com
+hostname = ap?.biliapi.net, ap?.bilibili.com
 
 **************************/
 
